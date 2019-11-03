0f2595d786607e2a75bc20f284a52721ab46c43a
deepakkumarlakshmipathi <Deepak.lakshmipathi550@gmail.com>
2019-11-03 11:32:01 -0500

    streaming - class- auth

--- src/main/scala/stream/tweet/Cars_Stream.scala
+++ src/main/scala/stream/tweet/Cars_Stream.scala
@@ -6,21 +6,28 @@ import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
 import twitter4j._
 
-object auth{
+class auth(val cK: String,val cSK: String, val aT: String, val aST: String) {
   val config = new twitter4j.conf.ConfigurationBuilder()
-    .setOAuthConsumerKey("")
-    .setOAuthConsumerSecret("")
-    .setOAuthAccessToken("")
-    .setOAuthAccessTokenSecret("")
+    .setOAuthConsumerKey(cK)
+    .setOAuthConsumerSecret(cSK)
+    .setOAuthAccessToken(aT)
+    .setOAuthAccessTokenSecret(aST)
     .build
-  val twitter_auth = new TwitterFactory(auth.config)
-  val a = new twitter4j.auth.OAuthAuthorization(auth.config)
+  val twitter_auth = new TwitterFactory(config)
+  val a = new twitter4j.auth.OAuthAuthorization(config)
   val atwitter =  twitter_auth.getInstance(a).getAuthorization()
 }
 
 
 object Cars_Stream {
   def main(args: Array[String]): Unit = {
+
+    if (args.length < 4){
+      println("need more args")
+      System.exit(1)
+    }
+
+    val auth = new auth(args(0),args(1),args(2),args(3))
     val filter: Array[String] = Array("Cars")
     val Session1 = SparkSession.builder().appName("Streaming_test").getOrCreate()
     val conf = new SparkConf().setAppName("Streaming_test")
@@ -29,8 +36,16 @@ object Cars_Stream {
     val st = TwitterUtils.createStream(ssc,Some(auth.atwitter),filter)
 
     val hashTags = st.flatMap(x => x.getText.split(" ").filter(_.startsWith("#")))
-    val top_10 = hashTags.map((_,1)).reduceByKeyAndWindow(_+_, Seconds(60))
+    val top_10 = hashTags.map((_,1)).reduceByKeyAndWindow(_+_, Seconds(10))
       .map{case (topic,count)=> (count,topic)}.transform(_.sortByKey(false))
+
+    top_10.foreachRDD(rdd => {
+      val top_List = rdd.take(10)
+      println("number of topics (%s total):".format(rdd.count()))
+      top_List.foreach{case (count,tag)=> println("Hashtag :%s(%s tweets)".format(tag,count))}
+    })
   }
 
 }
+
+
