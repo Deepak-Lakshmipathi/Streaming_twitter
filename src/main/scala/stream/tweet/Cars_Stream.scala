c566d6d2cb14352f9923f2e16f5989b373a9a6ba
deepakkumarlakshmipathi <Deepak.lakshmipathi550@gmail.com>
2019-11-04 21:32:32 -0500

    streaming - streaming System.setProperty

--- src/main/scala/stream/tweet/Cars_Stream.scala
+++ src/main/scala/stream/tweet/Cars_Stream.scala
@@ -34,7 +34,7 @@ object Cars_Stream {
       System.exit(1)
     }
 
-    val cK,cS,aT,aTS = (args(0),args(1),args(2),args(3))
+    val cK,cS,aT,aTS = args.take(4)
 
     System.setProperty("twitter4j.oauth.consumerKey",cK)
     System.setProperty("twitter4j.oauth.consumerSecret",cS)
